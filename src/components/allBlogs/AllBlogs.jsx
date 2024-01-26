60d99aaf37a4f00eab125330a47c549be18f1d40
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-01-26 11:19:35 -0300

    Exercise 5.9.

--- src/components/allBlogs/AllBlogs.jsx
+++ src/components/allBlogs/AllBlogs.jsx
@@ -14,7 +14,7 @@ const AllBlogs = ({ blogs, setBlogs }) => {
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog => (
+      {blogs.sort((a,b)=> b.likes - a.likes).map(blog => (
         <Blog key={blog.id} blog={blog} updateBlog={updateBlogSubmit} />
       ))}
     </div>
